737c3a5c1aff4ddc71ec6c245be1b7f881d3f99f
YuanLiu <13328332+yuanliu666@users.noreply.github.com>
2022-11-17 10:22:02 -0800

    Add refresh button.

--- app/src/main/java/com/liu966/dogapi/MainActivity.kt
+++ app/src/main/java/com/liu966/dogapi/MainActivity.kt
@@ -3,11 +3,21 @@ package com.liu966.dogapi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.liu966.dogapi.compose.NetworkImage
 import com.liu966.dogapi.data.APIClient
 import com.liu966.dogapi.data.DogAPI
@@ -27,21 +37,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    RandomDogImage()
+                    val coroutineScope = rememberCoroutineScope()
+                    val url = remember { mutableStateOf("") }
+                    LaunchedEffect(key1 = Unit) {
+                        coroutineScope.launch {
+                            url.value = dogApi.getRandomSingleDogImage().body()?.message ?: ""
+                        }
+                    }
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        NetworkImage(url = url.value, modifier = Modifier.fillMaxSize())
+                        Button(onClick = {
+                            coroutineScope.launch {
+                                url.value = dogApi.getRandomSingleDogImage().body()?.message ?: ""
+                            }
+                        },
+                            modifier = Modifier
+                                .align(Alignment.BottomCenter)
+                                .padding(bottom = 16.dp)
+                        ) {
+                            Text(text = "Refresh", color = Color.White)
+                        }
+                    }
                 }
             }
         }
-    }
-
-    @Composable
-    fun RandomDogImage() {
-        val coroutineScope = rememberCoroutineScope()
-        val url = remember { mutableStateOf("") }
-        LaunchedEffect(key1 = Unit) {
-            coroutineScope.launch {
-                url.value = dogApi.getRandomSingleDogImage().body()?.message ?: ""
-            }
-        }
-        NetworkImage(url = url.value, modifier = Modifier.fillMaxSize())
     }
 }
